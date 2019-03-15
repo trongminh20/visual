cb4b9be53c058a0d77eb0d62b7c8f4d0f76452a3
phan minh <trongminh20@gmail.com>
2019-03-15 00:12:34 -0700

    add lab 7 rolling dice

---
+++ MinhPhanLab7/DiceApp/DiceAppFRM.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class DiceAppFRM
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.numDiceGBX = New System.Windows.Forms.GroupBox()
+        Me.threeRBN = New System.Windows.Forms.RadioButton()
+        Me.twoRBN = New System.Windows.Forms.RadioButton()
+        Me.oneRBN = New System.Windows.Forms.RadioButton()
+        Me.die1PBX = New System.Windows.Forms.PictureBox()
+        Me.die2PBX = New System.Windows.Forms.PictureBox()
+        Me.die3PBX = New System.Windows.Forms.PictureBox()
+        Me.diceGBX = New System.Windows.Forms.GroupBox()
+        Me.rollBTN = New System.Windows.Forms.Button()
+        Me.numDiceGBX.SuspendLayout()
+        CType(Me.die1PBX, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.die2PBX, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.die3PBX, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.diceGBX.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'numDiceGBX
+        '
+        Me.numDiceGBX.Controls.Add(Me.threeRBN)
+        Me.numDiceGBX.Controls.Add(Me.twoRBN)
+        Me.numDiceGBX.Controls.Add(Me.oneRBN)
+        Me.numDiceGBX.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.numDiceGBX.Location = New System.Drawing.Point(24, 34)
+        Me.numDiceGBX.Margin = New System.Windows.Forms.Padding(6)
+        Me.numDiceGBX.Name = "numDiceGBX"
+        Me.numDiceGBX.Padding = New System.Windows.Forms.Padding(6)
+        Me.numDiceGBX.Size = New System.Drawing.Size(166, 238)
+        Me.numDiceGBX.TabIndex = 0
+        Me.numDiceGBX.TabStop = False
+        Me.numDiceGBX.Text = "Number Of Dice"
+        '
+        'threeRBN
+        '
+        Me.threeRBN.AutoSize = True
+        Me.threeRBN.Location = New System.Drawing.Point(25, 158)
+        Me.threeRBN.Name = "threeRBN"
+        Me.threeRBN.Size = New System.Drawing.Size(95, 22)
+        Me.threeRBN.TabIndex = 2
+        Me.threeRBN.TabStop = True
+        Me.threeRBN.Text = "Three dice"
+        Me.threeRBN.UseVisualStyleBackColor = True
+        '
+        'twoRBN
+        '
+        Me.twoRBN.AutoSize = True
+        Me.twoRBN.Location = New System.Drawing.Point(25, 98)
+        Me.twoRBN.Name = "twoRBN"
+        Me.twoRBN.Size = New System.Drawing.Size(86, 22)
+        Me.twoRBN.TabIndex = 1
+        Me.twoRBN.TabStop = True
+        Me.twoRBN.Text = "Two dice"
+        Me.twoRBN.UseVisualStyleBackColor = True
+        '
+        'oneRBN
+        '
+        Me.oneRBN.AutoSize = True
+        Me.oneRBN.Location = New System.Drawing.Point(24, 48)
+        Me.oneRBN.Name = "oneRBN"
+        Me.oneRBN.Size = New System.Drawing.Size(85, 22)
+        Me.oneRBN.TabIndex = 0
+        Me.oneRBN.TabStop = True
+        Me.oneRBN.Text = "One dice"
+        Me.oneRBN.UseVisualStyleBackColor = True
+        '
+        'die1PBX
+        '
+        Me.die1PBX.Location = New System.Drawing.Point(24, 40)
+        Me.die1PBX.Name = "die1PBX"
+        Me.die1PBX.Size = New System.Drawing.Size(100, 100)
+        Me.die1PBX.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.die1PBX.TabIndex = 1
+        Me.die1PBX.TabStop = False
+        '
+        'die2PBX
+        '
+        Me.die2PBX.Location = New System.Drawing.Point(152, 80)
+        Me.die2PBX.Name = "die2PBX"
+        Me.die2PBX.Size = New System.Drawing.Size(100, 100)
+        Me.die2PBX.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.die2PBX.TabIndex = 2
+        Me.die2PBX.TabStop = False
+        '
+        'die3PBX
+        '
+        Me.die3PBX.Location = New System.Drawing.Point(280, 120)
+        Me.die3PBX.Name = "die3PBX"
+        Me.die3PBX.Size = New System.Drawing.Size(100, 100)
+        Me.die3PBX.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.die3PBX.TabIndex = 3
+        Me.die3PBX.TabStop = False
+        '
+        'diceGBX
+        '
+        Me.diceGBX.Controls.Add(Me.die2PBX)
+        Me.diceGBX.Controls.Add(Me.die3PBX)
+        Me.diceGBX.Controls.Add(Me.die1PBX)
+        Me.diceGBX.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.diceGBX.Location = New System.Drawing.Point(221, 34)
+        Me.diceGBX.Name = "diceGBX"
+        Me.diceGBX.Size = New System.Drawing.Size(404, 238)
+        Me.diceGBX.TabIndex = 4
+        Me.diceGBX.TabStop = False
+        Me.diceGBX.Text = "Roll the dice"
+        '
+        'rollBTN
+        '
+        Me.rollBTN.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rollBTN.Location = New System.Drawing.Point(430, 295)
+        Me.rollBTN.Name = "rollBTN"
+        Me.rollBTN.Size = New System.Drawing.Size(195, 46)
+        Me.rollBTN.TabIndex = 5
+        Me.rollBTN.Text = "Roll!"
+        Me.rollBTN.UseVisualStyleBackColor = True
+        '
+        'DiceAppFRM
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(649, 367)
+        Me.Controls.Add(Me.rollBTN)
+        Me.Controls.Add(Me.diceGBX)
+        Me.Controls.Add(Me.numDiceGBX)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(6)
+        Me.Name = "DiceAppFRM"
+        Me.Text = "Dice Roller"
+        Me.numDiceGBX.ResumeLayout(False)
+        Me.numDiceGBX.PerformLayout()
+        CType(Me.die1PBX, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.die2PBX, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.die3PBX, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.diceGBX.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents numDiceGBX As GroupBox
+    Friend WithEvents die1PBX As PictureBox
+    Friend WithEvents die2PBX As PictureBox
+    Friend WithEvents die3PBX As PictureBox
+    Friend WithEvents diceGBX As GroupBox
+    Friend WithEvents rollBTN As Button
+    Friend WithEvents threeRBN As RadioButton
+    Friend WithEvents twoRBN As RadioButton
+    Friend WithEvents oneRBN As RadioButton
+End Class
---
+++ MinhPhanLab7/DiceApp/DiceAppFRM.vb
@@ -0,0 +1,116 @@
+﻿Option Strict On
+Public Class DiceAppFRM
+    Private Function getMessage(x As Integer, y As Integer) As String
+        Dim message As String = ""
+        If (y = 2) Then
+            Select Case x
+                Case 2
+                    message &= "You rolled Snake Eyes!"
+                Case 3
+                    message &= "You rolled Trey!"
+                Case 7
+                    message &= "You rolled Lucky Seven!"
+                Case 11
+                    message &= "You rolled Yo Eleven!"
+                Case 12
+                    message &= "You rolled Box Cars!"
+                Case Else
+                    message &= "You rolled a total of " & x.ToString
+            End Select
+        ElseIf (y <> 2) Then
+            message &= "You rolled a total of " & x.ToString
+        End If
+        Return message
+    End Function
+
+    Private Function rollDice(numDice As Integer) As Integer
+        Dim random As Random = New Random
+        Dim dice1 As Integer = random.Next(0, 7)
+        Dim dice2 As Integer = random.Next(0, 7)
+        Dim dice3 As Integer = random.Next(0, 7)
+        Dim total As Integer
+
+        Select Case numDice
+            Case 1
+                die1PBX.Image = Image.FromFile("Images\die" & dice1.ToString & ".png")
+
+                total = dice1
+            Case 2
+                die1PBX.Image = Image.FromFile("Images\die" & dice1.ToString & ".png")
+                die2PBX.Image = Image.FromFile("Images\die" & dice2.ToString & ".png")
+
+                total = dice1 + dice2
+            Case 3
+                die1PBX.Image = Image.FromFile("Images\die" & dice1.ToString & ".png")
+                die2PBX.Image = Image.FromFile("Images\die" & dice2.ToString & ".png")
+                die3PBX.Image = Image.FromFile("Images\die" & dice3.ToString & ".png")
+
+                total = dice1 + dice2 + dice3
+        End Select
+
+        Return total
+    End Function
+
+    Private Sub ImagesReset()
+        die1PBX.Image = Image.FromFile("Images\dieFooBar.png")
+        die2PBX.Image = Image.FromFile("Images\dieFooBar.png")
+        die3PBX.Image = Image.FromFile("Images\dieFooBar.png")
+        diceGBX.Text = "Roll the dice"
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles oneRBN.CheckedChanged
+        If (oneRBN.Checked) Then
+            die1PBX.Location = New Point(125, 50)
+            die1PBX.Size = New Drawing.Size(150, 150)
+
+            die1PBX.Visible = True
+            die2PBX.Visible = False
+            die3PBX.Visible = False
+
+            ImagesReset()
+        End If
+    End Sub
+
+    Private Sub twoRBN_CheckedChanged(sender As Object, e As EventArgs) Handles twoRBN.CheckedChanged
+        If (twoRBN.Checked) Then
+            die1PBX.Location = New Point(80, 40)
+            die2PBX.Location = New Point(220, 120)
+            die1PBX.Size = New Drawing.Size(100, 100)
+
+            die1PBX.Visible = True
+            die2PBX.Visible = True
+            die3PBX.Visible = False
+
+            ImagesReset()
+        End If
+    End Sub
+
+    Private Sub threeRBN_CheckedChanged(sender As Object, e As EventArgs) Handles threeRBN.CheckedChanged
+        If (threeRBN.Checked) Then
+            die1PBX.Location = New Point(24, 40)
+            die1PBX.Size = New Drawing.Size(100, 100)
+            die2PBX.Location = New Point(152, 80)
+            die3PBX.Location = New Point(280, 120)
+
+            die1PBX.Visible = True
+            die2PBX.Visible = True
+            die3PBX.Visible = True
+
+            ImagesReset()
+        End If
+    End Sub
+    Private Sub rollBTN_Click(sender As Object, e As EventArgs) Handles rollBTN.Click
+        Select Case True
+            Case oneRBN.Checked
+                rollDice(1)
+                diceGBX.Text = getMessage(rollDice(1), 1)
+            Case twoRBN.Checked
+                rollDice(2)
+                diceGBX.Text = getMessage(rollDice(2), 2)
+            Case threeRBN.Checked
+                rollDice(3)
+                diceGBX.Text = getMessage(rollDice(3), 3)
+        End Select
+    End Sub
+
+End Class
